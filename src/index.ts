1fd8755fdb8f4e84ac0481a0bcd2d130d69f3036
valentimdev <valentimdesenvolvedor@gmail.com>
2025-11-04 16:48:45 -0300

    adicionando front qualquer pra testar o back

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,40 @@
-import Fastify from 'fastify'
-import "reflect-metadata";
+import Fastify from 'fastify';
+import 'reflect-metadata';
 import { AppDataSource } from './config/data-source.js';
 import { userRoutes } from './modules/user/User.Controller.js';
 import fastifyJwt from '@fastify/jwt';
 import dotenv from 'dotenv';
 import { authRoutes } from './modules/auth/Auth.Controller.js';
+import fastifyCors from '@fastify/cors';
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
+fastify.register(fastifyCors, {
+  origin: '*',
 
-dotenv.config()
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
+dotenv.config();
 
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+  return { hello: 'world' };
+});
 
 fastify.register(userRoutes);
-fastify.register(authRoutes)
-fastify.register(fastifyJwt,{
-  secret:process.env.JWT_SECRET!,
-
-})
+fastify.register(authRoutes);
+fastify.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET!,
+});
 const start = async () => {
   try {
     await AppDataSource.initialize();
-    fastify.log.info("Data Source inicializado com sucesso!");
-    await fastify.listen({ port: 3000 })
+    fastify.log.info('Data Source inicializado com sucesso!');
+    await fastify.listen({ port: 3000 });
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
-start()
+};
+start();
